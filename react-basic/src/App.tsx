018ee4b4b41fcd807ee370c8f2152fbfb9ed56ef
Phornhathai <phornhathai.aum@gmail.com>
2023-12-02 17:40:25 +0700

    Submit

---
+++ react-basic/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import SignUp from './components/signUp'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <SignUp/>
+    </>
+  )
+}
+
+export default App
